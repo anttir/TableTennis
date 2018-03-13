346dbe98a4f78b7446fb1063ee2c853f246de73c
Jukka-Pekka Louneva <jukkapekka.louneva@gmail.com>
2018-03-13 20:19:13 +0200

    Run nuxt server build process only in dev

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const nuxt = new Nuxt(require('./nuxt.config.js'));
 
 app.use(nuxt.render);
 
-const isDev = true;
+const isDev = process.env.NODE_ENV !== 'prodution';
 // Build only in test mode
 if (isDev) {
     const builder = new Builder(nuxt);
