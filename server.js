506cbdc5759a8266a74afcdf0d3715b7595ef164
Antti <antti.rasi@m2ms.eu>
2018-04-26 20:21:59 +0300

    tiedoston tyylittely

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const express = require('express')
-const app = express()
-const {
-    Nuxt,
-    Builder
-} = require('nuxt');
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const { Nuxt, Builder } = require("nuxt");
+const port = process.env.PORT || 3000;
 
-const nuxt = new Nuxt(require('./nuxt.config.js'));
+const nuxt = new Nuxt(require("./nuxt.config.js"));
 
 app.use(nuxt.render);
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== "production";
 // Build only in test mode
 if (isDev) {
-    const builder = new Builder(nuxt);
-    builder.build().then(() => {
-        console.log('Nuxt built')
-    });
+  const builder = new Builder(nuxt);
+  builder.build().then(() => {
+    console.log("Nuxt built");
+  });
 } else {
-    console.log('Nuxt built')
+  console.log("Nuxt built");
 }
-app.listen(port, () => console.log('Example app listening on port ' + port + "!"))
+app.listen(port, () =>
+  console.log("Example app listening on port " + port + "!")
+);
